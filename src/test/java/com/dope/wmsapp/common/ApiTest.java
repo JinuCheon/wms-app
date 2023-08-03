ff8b3668e3fa7148633334b6cc4be0cf3ac139fa
JinuCheon <woojin8787@gmail.com>
2023-08-03 17:11:20 +0900

    14.database cleaner 추가

--- src/test/java/com/dope/wmsapp/common/ApiTest.java
+++ src/test/java/com/dope/wmsapp/common/ApiTest.java
@@ -2,6 +2,7 @@ package com.dope.wmsapp.common;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
@@ -10,11 +11,14 @@ public class ApiTest {
 
     @LocalServerPort
     private int port;
+    @Autowired DatabaseCleaner databaseCleaner;
 
     @BeforeEach
     void setUp() {
         if(RestAssured.UNDEFINED_PORT == RestAssured.port) {
             RestAssured.port = port;
+            databaseCleaner.afterPropertiesSet();
         }
+        databaseCleaner.execute();
     }
 }
