de6a48f5236b3abdc2aa20b9cb0443cfd18b35ab
JinuCheon <woojin8787@gmail.com>
2023-08-03 17:22:58 +0900

    Revert "001-상품등록"

--- src/main/java/com/dope/wmsapp/product/domain/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package com.dope.wmsapp.product.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
--- src/test/java/com/dope/wmsapp/common/Scenario.java
+++
@@ -1,9 +0,0 @@
-package com.dope.wmsapp.common;
-
-import com.dope.wmsapp.product.feature.api.RegisterProductApi;
-
-public class Scenario {
-    public static RegisterProductApi registerProduct() {
-        return new RegisterProductApi();
-    }
-}
