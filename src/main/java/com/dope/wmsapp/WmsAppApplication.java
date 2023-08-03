d8a0404661d1d433d41062112e9305984d371327
JinuCheon <woojin8787@gmail.com>
2023-08-03 11:16:41 +0900

    init

---
+++ src/main/java/com/dope/wmsapp/WmsAppApplication.java
@@ -0,0 +1,13 @@
+package com.dope.wmsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WmsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dope/wmsapp/WmsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dope.wmsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
