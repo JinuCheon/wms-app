9e8a5bb79d6456c18a364fb63c2a6a97c213011f
JinuCheon <woojin8787@gmail.com>
2023-08-03 11:39:33 +0900

    2.create test case registerProduct

--- src/test/java/com/dope/wmsapp/product/feature/RegisterProductTest.java
+++ src/test/java/com/dope/wmsapp/product/feature/RegisterProductTest.java
@@ -1,5 +1,13 @@
 package com.dope.wmsapp.product.feature;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class RegisterProductTest {
 
+    @Test
+    @DisplayName("상품을 등록한다.")
+    void registerProduct() {
+
+    }
 }
