bad46ea32e1aaece4ad7c302fbc9a3af869b33ee
JinuCheon <woojin8787@gmail.com>
2023-08-10 21:52:52 +0900

    4. create method request

--- src/test/java/com/dope/wmsapp/location/feature/RegisterLocationTest.java
+++ src/test/java/com/dope/wmsapp/location/feature/RegisterLocationTest.java
@@ -1,13 +1,27 @@
 package com.dope.wmsapp.location.feature;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RegisterLocationTest {
 
+    private RegisterLocation registerLocation;
+
+    @BeforeEach
+    void setUp() {
+        registerLocation = new RegisterLocation();
+    }
+
     @Test
     @DisplayName("로케이션을 등록한다.")
     void registerLocation() {
+        registerLocation.request();
+    }
 
+    private class RegisterLocation {
+        public void request() {
+
+        }
     }
 }
