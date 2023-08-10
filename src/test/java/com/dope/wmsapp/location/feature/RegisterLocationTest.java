aa8ca937875074910711ff88d9c21fd234d1e518
JinuCheon <woojin8787@gmail.com>
2023-08-10 20:57:36 +0900

    2. create test case RegisterLocation

--- src/test/java/com/dope/wmsapp/location/feature/RegisterLocationTest.java
+++ src/test/java/com/dope/wmsapp/location/feature/RegisterLocationTest.java
@@ -1,6 +1,13 @@
 package com.dope.wmsapp.location.feature;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RegisterLocationTest {
+
+    @Test
+    @DisplayName("로케이션을 등록한다.")
+    void registerLocation() {
+
+    }
 }
