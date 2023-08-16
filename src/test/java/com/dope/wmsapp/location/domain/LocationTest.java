1ed00d36ce91ce307d7a7f67627a35b5be92fa19
JinuCheon <woojin8787@gmail.com>
2023-08-17 06:52:44 +0900

    16. create test case

--- src/test/java/com/dope/wmsapp/location/domain/LocationTest.java
+++ src/test/java/com/dope/wmsapp/location/domain/LocationTest.java
@@ -1,10 +1,26 @@
 package com.dope.wmsapp.location.domain;
 
+import com.dope.wmsapp.inbound.domain.InboundItemFixture;
+import com.dope.wmsapp.inbound.domain.LPN;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
 
 class LocationTest {
 
     @Test
     void assignLPN() {
+        final String locationBarcode = "A-1-1";
+        final StorageType storageType = StorageType.TOTE;
+        final UsagePurpose usagePurpose = UsagePurpose.MOVE;
+        final Location location = new Location(locationBarcode, storageType, usagePurpose);
+
+        final String lpnBarcode = "LPN-1";
+        final LocalDateTime expirationAt = LocalDateTime.now().plusDays(1);
+        final LPN lpn = new LPN(lpnBarcode, expirationAt, InboundItemFixture.anInboundItem().build());
+
+        location.assignLPN(lpn);
+
+
     }
 }
