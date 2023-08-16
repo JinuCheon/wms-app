8e20d65026768dfc56571a9db041d6909e19f400
JinuCheon <woojin8787@gmail.com>
2023-08-17 07:34:14 +0900

    23. assignLPN 이 두 번 호출되었을때의 테스트 추가

--- src/test/java/com/dope/wmsapp/location/domain/LocationTest.java
+++ src/test/java/com/dope/wmsapp/location/domain/LocationTest.java
@@ -19,4 +19,16 @@ class LocationTest {
         assertThat(location.getLocationLPNList().get(0).getInventoryQuantity()).isEqualTo(1L);
     }
 
+    @Test
+    void already_exist_assignLPN() {
+        final Location location = LocationFixture.aLocation().build();
+        final LPN lpn = LPNFixture.anLPN().build();
+
+        location.assignLPN(lpn);
+        location.assignLPN(lpn);
+
+        assertThat(location.getLocationLPNList()).hasSize(1);
+        assertThat(location.getLocationLPNList().get(0).getInventoryQuantity()).isEqualTo(2L);
+    }
+
 }
