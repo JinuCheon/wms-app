f7533384141911f2c71aab5c1ce1031d2066d677
JinuCheon <woojin8787@gmail.com>
2023-08-16 20:03:40 +0900

    18. change request validation to jakarta validate

--- src/main/java/com/dope/wmsapp/location/feature/RegisterLocation.java
+++ src/main/java/com/dope/wmsapp/location/feature/RegisterLocation.java
@@ -4,9 +4,11 @@ import com.dope.wmsapp.location.domain.Location;
 import com.dope.wmsapp.location.domain.LocationRepository;
 import com.dope.wmsapp.location.domain.StorageType;
 import com.dope.wmsapp.location.domain.UsagePurpose;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -19,20 +21,18 @@ public class RegisterLocation {
 
     @PostMapping("/locations")
     @ResponseStatus(HttpStatus.CREATED)
-    public void request(@RequestBody final Request request) {
+    public void request(@RequestBody @Valid final Request request) {
         final Location location = request.toDomain();
         locationRepository.save(location);
     }
 
-    public record Request(String locationBarcode,
-                          StorageType storageType,
-                          UsagePurpose usagePurpose) {
-        public Request {
-            Assert.hasText(locationBarcode, "로케이션 바코드는 필수입니다.");
-            Assert.notNull(storageType, "로케이션 타입은 필수입니다.");
-            Assert.notNull(usagePurpose, "보관 목적은 필수입니다.");
-        }
-
+    public record Request(
+            @NotBlank(message = "로케이션 바코드는 필수입니다.")
+            String locationBarcode,
+            @NotNull(message = "로케이션 타입은 필수입니다.")
+            StorageType storageType,
+            @NotNull(message = "보관 목적은 필수입니다.")
+            UsagePurpose usagePurpose) {
         public Location toDomain() {
             return new Location(
                     locationBarcode,
