821d693aa7e650af180e4987f04bd1620178e7e3
JinuCheon <woojin8787@gmail.com>
2023-08-16 20:45:34 +0900

    19. location domain 을 jpa로 변경

--- src/main/java/com/dope/wmsapp/location/domain/Location.java
+++ src/main/java/com/dope/wmsapp/location/domain/Location.java
@@ -1,12 +1,37 @@
 package com.dope.wmsapp.location.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
 import org.springframework.util.Assert;
 
+@Entity
+@Getter
+@Table(name = "location")
+@Comment("로케이션")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Location {
-    private final String locationBarcode;
-    private final StorageType storageType;
-    private final UsagePurpose usagePurpose;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "location_no", nullable = false)
     private Long locationNo;
+    @Column(name = "location_barcode", nullable = false)
+    private String locationBarcode;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "storage_type", nullable = false)
+    private StorageType storageType;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "usage_purpose", nullable = false)
+    private UsagePurpose usagePurpose;
 
     public Location(final String locationBarcode, final StorageType storageType, final UsagePurpose usagePurpose) {
         validateConstructor(locationBarcode, storageType, usagePurpose);
