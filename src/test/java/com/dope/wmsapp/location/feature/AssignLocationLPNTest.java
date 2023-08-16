692b922a183a93e2eb570c252807e229ae833ac7
JinuCheon <woojin8787@gmail.com>
2023-08-17 03:58:09 +0900

    2. create test case: assignLocationLPN

--- src/test/java/com/dope/wmsapp/location/feature/AssignLocationLPNTest.java
+++ src/test/java/com/dope/wmsapp/location/feature/AssignLocationLPNTest.java
@@ -1,4 +1,13 @@
 package com.dope.wmsapp.location.feature;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class AssignLocationLPNTest {
+
+    @Test
+    @DisplayName("로케이션에 LPN 할당")
+    void assignLocationLPN() {
+        
+    }
 }
