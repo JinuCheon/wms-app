992b5c9a23b450861b4876158fb4dba25ace7956
JinuCheon <woojin8787@gmail.com>
2023-08-17 06:55:59 +0900

    17. extract method

--- src/test/java/com/dope/wmsapp/location/domain/LocationTest.java
+++ src/test/java/com/dope/wmsapp/location/domain/LocationTest.java
@@ -10,17 +10,27 @@ class LocationTest {
 
     @Test
     void assignLPN() {
-        final String locationBarcode = "A-1-1";
-        final StorageType storageType = StorageType.TOTE;
-        final UsagePurpose usagePurpose = UsagePurpose.MOVE;
-        final Location location = new Location(locationBarcode, storageType, usagePurpose);
+        final Location location = createLocation();
 
-        final String lpnBarcode = "LPN-1";
-        final LocalDateTime expirationAt = LocalDateTime.now().plusDays(1);
-        final LPN lpn = new LPN(lpnBarcode, expirationAt, InboundItemFixture.anInboundItem().build());
+        final LPN lpn = createLpn();
 
         location.assignLPN(lpn);
 
 
+    }
+
+    private LPN createLpn() {
+        final String lpnBarcode = "LPN-1";
+        final LocalDateTime expirationAt = LocalDateTime.now().plusDays(1);
+        final LPN lpn = new LPN(lpnBarcode, expirationAt, InboundItemFixture.anInboundItem().build());
+        return lpn;
+    }
+
+    private Location createLocation() {
+        final String locationBarcode = "A-1-1";
+        final StorageType storageType = StorageType.TOTE;
+        final UsagePurpose usagePurpose = UsagePurpose.MOVE;
+        final Location location = new Location(locationBarcode, storageType, usagePurpose);
+        return location;
     }
 }
