6d646de7994eaeaea170dc4a6806c0d2eef4110e
JinuCheon <woojin8787@gmail.com>
2023-08-17 04:48:49 +0900

    8. create method findbylocationmethod

--- src/main/java/com/dope/wmsapp/location/domain/LocationRepository.java
+++ src/main/java/com/dope/wmsapp/location/domain/LocationRepository.java
@@ -2,5 +2,8 @@ package com.dope.wmsapp.location.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LocationRepository extends JpaRepository<Location, Long> {
+    Optional<Location> findByLocationBarcode(String locationBarcode);
 }
--- src/test/java/com/dope/wmsapp/location/feature/AssignLocationLPNTest.java
+++ src/test/java/com/dope/wmsapp/location/feature/AssignLocationLPNTest.java
@@ -1,5 +1,7 @@
 package com.dope.wmsapp.location.feature;
 
+import com.dope.wmsapp.location.domain.Location;
+import com.dope.wmsapp.location.domain.LocationRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.util.Assert;
@@ -16,11 +18,17 @@ class AssignLocationLPNTest {
                 locationBarcode,
                 lpnBarcode
         );
+
         assignLocationLPN.request(request);
+
+
     }
 
     private class AssignLocationLPN {
+        private LocationRepository locationRepository;
+
         public void request(final Request request) {
+            final Location location = locationRepository.findByLocationBarcode(request.locationBarcode).orElseThrow();
         }
 
         public record Request(String locationBarcode, String lpnBarcode) {
