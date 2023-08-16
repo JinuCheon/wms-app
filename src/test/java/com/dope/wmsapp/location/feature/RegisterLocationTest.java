15af3cc6af7b662c46a3d4de9bdf967a4a5e1866
JinuCheon <woojin8787@gmail.com>
2023-08-16 19:29:42 +0900

    17. change to scenario test

--- src/test/java/com/dope/wmsapp/location/feature/RegisterLocationTest.java
+++ src/test/java/com/dope/wmsapp/location/feature/RegisterLocationTest.java
@@ -3,13 +3,9 @@ package com.dope.wmsapp.location.feature;
 import com.dope.wmsapp.common.ApiTest;
 import com.dope.wmsapp.common.Scenario;
 import com.dope.wmsapp.location.domain.LocationRepository;
-import com.dope.wmsapp.location.domain.StorageType;
-import com.dope.wmsapp.location.domain.UsagePurpose;
-import io.restassured.RestAssured;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,22 +18,6 @@ class RegisterLocationTest extends ApiTest {
     @DisplayName("로케이션을 등록한다.")
     void registerLocation() {
         Scenario.registerLocation().request();
-        final String locationBarcode = "A-1-1";
-        final StorageType storageType = StorageType.TOTE;
-        final UsagePurpose usagePurpose = UsagePurpose.MOVE;
-        final RegisterLocation.Request request = new RegisterLocation.Request(
-                locationBarcode,
-                storageType,
-                usagePurpose
-        );
-
-        RestAssured.given().log().all()
-                .body(request)
-                .contentType("application/json")
-                .when()
-                .post("http://localhost:8080/locations")
-                .then().log().all()
-                .statusCode(HttpStatus.CREATED.value());
 
         assertThat(locationRepository.findAll()).hasSize(1);
     }
