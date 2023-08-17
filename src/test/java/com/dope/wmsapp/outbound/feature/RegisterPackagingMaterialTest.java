cbff63c369be3597a1acd02a975f65e3f92eeb83
JinuCheon <woojin8787@gmail.com>
2023-08-17 19:11:12 +0900

    9. add field packagematarial

--- src/test/java/com/dope/wmsapp/outbound/feature/RegisterPackagingMaterialTest.java
+++ src/test/java/com/dope/wmsapp/outbound/feature/RegisterPackagingMaterialTest.java
@@ -24,6 +24,7 @@ class RegisterPackagingMaterialTest {
         final Long outerLengthInMillimeters = 1000L;
         final Long weightInGrams = 1000L;
         final Long maxWeightInGrams = 1000L;
+        final MaterialType materialType = MaterialType.CORRUGATED_BOX;
         final RegisterPackageMaterial.Request request = new RegisterPackageMaterial.Request(
                 "name",
                 "code",
@@ -35,7 +36,7 @@ class RegisterPackagingMaterialTest {
                 outerLengthInMillimeters,
                 weightInGrams,
                 maxWeightInGrams,
-                MaterialType.CORRUGATED_BOX
+                materialType
         );
         registerPackageMaterial.request(request);
 
@@ -43,6 +44,31 @@ class RegisterPackagingMaterialTest {
     }
 
     private static class PackagingMaterial {
+        private final String name;
+        private final String code;
+        private final RegisterPackageMaterial.Request.PackagingMaterialDimension packagingMaterialDimension;
+        private final Long weightInGrams;
+        private final Long maxWeightInGrams;
+        private final MaterialType materialType;
+
+        public PackagingMaterial(final String name, final String code, final RegisterPackageMaterial.Request.PackagingMaterialDimension packagingMaterialDimension, final Long weightInGrams, final Long maxWeightInGrams, final MaterialType materialType) {
+            validateConstructor(name, code, packagingMaterialDimension, weightInGrams, maxWeightInGrams, materialType);
+            this.name = name;
+            this.code = code;
+            this.packagingMaterialDimension = packagingMaterialDimension;
+            this.weightInGrams = weightInGrams;
+            this.maxWeightInGrams = maxWeightInGrams;
+            this.materialType = materialType;
+        }
+
+        private static void validateConstructor(final String name, final String code, final RegisterPackageMaterial.Request.PackagingMaterialDimension packagingMaterialDimension, final Long weightInGrams, final Long maxWeightInGrams, final MaterialType materialType) {
+            Assert.hasText(name, "name must not be empty");
+            Assert.hasText(code, "code must not be empty");
+            Assert.notNull(packagingMaterialDimension, "packagingMaterialDimension must not be null");
+            Assert.notNull(weightInGrams, "weightInGrams must not be null");
+            Assert.notNull(maxWeightInGrams, "maxWeightInGrams must not be null");
+            Assert.notNull(materialType, "materialType must not be null");
+        }
     }
 
     private class RegisterPackageMaterial {
@@ -60,7 +86,7 @@ class RegisterPackagingMaterialTest {
                        Long outerLengthInMillimeters,
                        Long weightInGrams,
                        Long maxWeightInGrams,
-                       MaterialType corrugatedBox) {
+                       MaterialType materialType) {
             Request {
                 Assert.hasText(name, "name must not be empty");
                 Assert.hasText(code, "code must not be empty");
@@ -96,11 +122,71 @@ class RegisterPackagingMaterialTest {
                 if (maxWeightInGrams < 1) {
                     throw new IllegalArgumentException("maxWeightInGrams must be greater than 1");
                 }
-                Assert.notNull(corrugatedBox, "corrugatedBox must not be null");
+                Assert.notNull(materialType, "corrugatedBox must not be null");
             }
 
             public PackagingMaterial toDomain() {
-                return null;
+                return new PackagingMaterial(
+                        name,
+                        code,
+                        new PackagingMaterialDimension(
+                                innerWidthInMillimeters,
+                                innerHeightInMillimeters,
+                                innerLengthInMillimeters,
+                                outerWidthInMillimeters,
+                                outerHeightInMillimeters,
+                                outerLengthInMillimeters
+                        ),
+                        weightInGrams,
+                        maxWeightInGrams,
+                        materialType
+                );
+            }
+
+            private class PackagingMaterialDimension {
+                private final Long innerWidthInMillimeters;
+                private final Long innerHeightInMillimeters;
+                private final Long innerLengthInMillimeters;
+                private final Long outerWidthInMillimeters;
+                private final Long outerHeightInMillimeters;
+                private final Long outerLengthInMillimeters;
+
+                public PackagingMaterialDimension(final Long innerWidthInMillimeters, final Long innerHeightInMillimeters, final Long innerLengthInMillimeters, final Long outerWidthInMillimeters, final Long outerHeightInMillimeters, final Long outerLengthInMillimeters) {
+                    validateConstructor(innerWidthInMillimeters, innerHeightInMillimeters, innerLengthInMillimeters, outerWidthInMillimeters, outerHeightInMillimeters, outerLengthInMillimeters);
+                    this.innerWidthInMillimeters = innerWidthInMillimeters;
+                    this.innerHeightInMillimeters = innerHeightInMillimeters;
+                    this.innerLengthInMillimeters = innerLengthInMillimeters;
+                    this.outerWidthInMillimeters = outerWidthInMillimeters;
+                    this.outerHeightInMillimeters = outerHeightInMillimeters;
+                    this.outerLengthInMillimeters = outerLengthInMillimeters;
+                }
+
+                private static void validateConstructor(final Long innerWidthInMillimeters, final Long innerHeightInMillimeters, final Long innerLengthInMillimeters, final Long outerWidthInMillimeters, final Long outerHeightInMillimeters, final Long outerLengthInMillimeters) {
+                    Assert.notNull(innerWidthInMillimeters, "innerWidthInMillimeters must not be null");
+                    if (innerWidthInMillimeters < 1) {
+                        throw new IllegalArgumentException("innerWidthInMillimeters must be greater than 1");
+                    }
+                    Assert.notNull(innerHeightInMillimeters, "innerHeightInMillimeters must not be null");
+                    if (innerHeightInMillimeters < 1) {
+                        throw new IllegalArgumentException("innerHeightInMillimeters must be greater than 1");
+                    }
+                    Assert.notNull(innerLengthInMillimeters, "innerLengthInMillimeters must not be null");
+                    if (innerLengthInMillimeters < 1) {
+                        throw new IllegalArgumentException("innerLengthInMillimeters must be greater than 1");
+                    }
+                    Assert.notNull(outerWidthInMillimeters, "outerWidthInMillimeters must not be null");
+                    if (outerWidthInMillimeters < 1) {
+                        throw new IllegalArgumentException("outerWidthInMillimeters must be greater than 1");
+                    }
+                    Assert.notNull(outerHeightInMillimeters, "outerHeightInMillimeters must not be null");
+                    if (outerHeightInMillimeters < 1) {
+                        throw new IllegalArgumentException("outerHeightInMillimeters must be greater than 1");
+                    }
+                    Assert.notNull(outerLengthInMillimeters, "outerLengthInMillimeters must not be null");
+                    if (outerLengthInMillimeters < 1) {
+                        throw new IllegalArgumentException("outerLengthInMillimeters must be greater than 1");
+                    }
+                }
             }
         }
     }
