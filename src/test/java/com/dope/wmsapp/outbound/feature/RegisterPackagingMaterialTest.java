672b87ea4b06e9615d7d51d91198c3a93f58d16a
JinuCheon <woojin8787@gmail.com>
2023-08-17 18:21:19 +0900

    8. add validation

--- src/test/java/com/dope/wmsapp/outbound/feature/RegisterPackagingMaterialTest.java
+++ src/test/java/com/dope/wmsapp/outbound/feature/RegisterPackagingMaterialTest.java
@@ -2,6 +2,7 @@ package com.dope.wmsapp.outbound.feature;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
 
 class RegisterPackagingMaterialTest {
 
@@ -43,7 +44,54 @@ class RegisterPackagingMaterialTest {
 
         }
 
-        public class Request {
+        record Request(String name,
+                       String code,
+                       Long innerWidthInMillimeters,
+                       Long innerHeightInMillimeters,
+                       Long innerLengthInMillimeters,
+                       Long outerWidthInMillimeters,
+                       Long outerHeightInMillimeters,
+                       Long outerLengthInMillimeters,
+                       Long weightInGrams,
+                       Long maxWeightInGrams,
+                       MaterialType corrugatedBox) {
+            Request {
+                Assert.hasText(name, "name must not be empty");
+                Assert.hasText(code, "code must not be empty");
+                Assert.notNull(innerWidthInMillimeters, "innerWidthInMillimeters must not be null");
+                if (innerWidthInMillimeters < 1) {
+                    throw new IllegalArgumentException("innerWidthInMillimeters must be greater than 1");
+                }
+                Assert.notNull(innerHeightInMillimeters, "innerHeightInMillimeters must not be null");
+                if (innerHeightInMillimeters < 1) {
+                    throw new IllegalArgumentException("innerHeightInMillimeters must be greater than 1");
+                }
+                Assert.notNull(innerLengthInMillimeters, "innerLengthInMillimeters must not be null");
+                if (innerLengthInMillimeters < 1) {
+                    throw new IllegalArgumentException("innerLengthInMillimeters must be greater than 1");
+                }
+                Assert.notNull(outerWidthInMillimeters, "outerWidthInMillimeters must not be null");
+                if (outerWidthInMillimeters < 1) {
+                    throw new IllegalArgumentException("outerWidthInMillimeters must be greater than 1");
+                }
+                Assert.notNull(outerHeightInMillimeters, "outerHeightInMillimeters must not be null");
+                if (outerHeightInMillimeters < 1) {
+                    throw new IllegalArgumentException("outerHeightInMillimeters must be greater than 1");
+                }
+                Assert.notNull(outerLengthInMillimeters, "outerLengthInMillimeters must not be null");
+                if (outerLengthInMillimeters < 1) {
+                    throw new IllegalArgumentException("outerLengthInMillimeters must be greater than 1");
+                }
+                Assert.notNull(weightInGrams, "weightInGrams must not be null");
+                if (weightInGrams < 1) {
+                    throw new IllegalArgumentException("weightInGrams must be greater than 1");
+                }
+                Assert.notNull(maxWeightInGrams, "maxWeightInGrams must not be null");
+                if (maxWeightInGrams < 1) {
+                    throw new IllegalArgumentException("maxWeightInGrams must be greater than 1");
+                }
+                Assert.notNull(corrugatedBox, "corrugatedBox must not be null");
+            }
         }
     }
 
