88fc0d1511dafe0da6165e73c9144cc5ea63beb2
JinuCheon <woojin8787@gmail.com>
2023-08-17 18:18:28 +0900

    6. change signature

--- src/test/java/com/dope/wmsapp/outbound/feature/RegisterPackagingMaterialTest.java
+++ src/test/java/com/dope/wmsapp/outbound/feature/RegisterPackagingMaterialTest.java
@@ -1,15 +1,40 @@
 package com.dope.wmsapp.outbound.feature;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RegisterPackagingMaterialTest {
 
+    RegisterPackageMaterial registerPackageMaterial;
+
+    @BeforeEach
+    void setUp() {
+        registerPackageMaterial = new RegisterPackageMaterial();
+    }
+
     @Test
     void registerPackagingMaterial() {
-
-        final RegisterPackageMaterial registerPackageMaterial = new RegisterPackageMaterial();
-
-        final RegisterPackageMaterial.Request request;
+        final Long innerWidthInMillimeters = 1000L;
+        final Long innerHeightInMillimeters = 1000L;
+        final Long innerLengthInMillimeters = 1000L;
+        final Long outerWidthInMillimeters = 1000L;
+        final Long outerHeightInMillimeters = 1000L;
+        final Long outerLengthInMillimeters = 1000L;
+        final Long weightInGrams = 1000L;
+        final Long maxWeightInGrams = 1000L;
+        final RegisterPackageMaterial.Request request = new RegisterPackageMaterial.Request(
+                "name",
+                "code",
+                innerWidthInMillimeters,
+                innerHeightInMillimeters,
+                innerLengthInMillimeters,
+                outerWidthInMillimeters,
+                outerHeightInMillimeters,
+                outerLengthInMillimeters,
+                weightInGrams,
+                maxWeightInGrams,
+                MaterialType.CORRUGATED_BOX
+        );
         registerPackageMaterial.request(request);
     }
 
@@ -19,6 +44,15 @@ class RegisterPackagingMaterialTest {
         }
 
         public class Request {
+        }
+    }
+
+    enum MaterialType {
+        CORRUGATED_BOX("골판지 상자");
+        private final String description;
+
+        MaterialType(final String description) {
+            this.description = description;
         }
     }
 }
